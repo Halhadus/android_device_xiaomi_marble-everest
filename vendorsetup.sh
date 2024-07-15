247004f1bb3835970f3e3ed580cf3d0a515d7378
Halhadus <eneshakans45@proton.me>
2024-07-15 07:36:28 +0000

    Update vendorsetup.sh
    Signed-off-by: Halhadus <eneshakans45@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,8 +11,10 @@ echo 'Cloning Device Common Tree'
 	git clone https://github.com/xiaomi-sm8450-marble/android_device_xiaomi_sm8450-common -b ursa device/xiaomi/sm8450-common
 
 echo 'Cloning MemeCamera Tree'
-	git clone https://github.com/xiaomi-sm8450-marble/android_device_xiaomi_miuicamera-marble.git -b ursa device/xiaomi/miuicamera-marble
-	git clone https://gitlab.com/SharmagRit/android_vendor_xiaomi_miuicamera-marble.git -b ursa vendor/xiaomi/miuicamera-marble
+	git clone https://github.com/Chaitanyakm/device_xiaomi_miuicamera-marble.git -b fourteen device/xiaomi/miuicamera-marble
+	git clone https://gitlab.com/Chaitanyakm/vendor_xiaomi_miuicamera-marble.git -b fourteen vendor/xiaomi/miuicamera-marble
+        rm -rf vendor/xiaomi/miuicamera-marble/proprietary/system/lib64/libcamera_algoup_jni.xiaomi.so
+        curl https://bitbucket.org/evo-x/vendor_xiaomi_miuicamera-marble/raw/7958b85c535c1c2e84be17c9e9025a86f2f81f13/proprietary/system/lib64/libcamera_algoup_jni.xiaomi.so -o vendor/xiaomi/miuicamera-marble/proprietary/system/lib64/libcamera_algoup_jni.xiaomi.so
 
 echo 'Cloning Hardware Display Tree'
 	git clone https://github.com/xiaomi-sm8450-marble/android_hardware_qcom_display -b ursa hardware/qcom-caf/sm8450/display
@@ -21,9 +23,9 @@ echo 'Cloning Hardware Xiaomi Tree'
 	git clone https://github.com/xiaomi-sm8450-marble/android_hardware_xiaomi -b ursa hardware/xiaomi
 
 echo 'Cloning Kernel Tree'
-	git clone --depth=1 https://github.com/xiaomi-sm8450-marble/android_kernel_xiaomi_sm8450 -b ursa kernel/xiaomi/sm8450
-	git clone --depth=1 https://github.com/xiaomi-sm8450-marble/android_kernel_xiaomi_sm8450-modules -b ursa kernel/xiaomi/sm8450-modules
-	git clone --depth=1 https://github.com/xiaomi-sm8450-marble/android_kernel_xiaomi_sm8450-devicetrees -b ursa kernel/xiaomi/sm8450-devicetrees
+	git clone --depth=1 https://github.com/Halhadus/android_kernel_xiaomi_sm8450 -b lineage-21 kernel/xiaomi/sm8450
+	git clone --depth=1 https://github.com/cupid-development/android_kernel_xiaomi_sm8450-modules -b lineage-21 kernel/xiaomi/sm8450-modules
+	git clone --depth=1 https://github.com/cupid-development/android_kernel_xiaomi_sm8450-devicetrees -b lineage-21 kernel/xiaomi/sm8450-devicetrees
 
 echo 'Cloning Vendor Tree'
 	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_xiaomi_marble -b ursa vendor/xiaomi/marble
@@ -33,3 +35,9 @@ echo 'Cloning Vendor Common Tree'
 
 echo 'Cloning Vendor Everest Signing Keys'
         git clone https://github.com/xiaomi-sm8450-marble/android_vendor_everest_signing_keys -b ursa vendor/everest/signing/keys
+
+echo 'Fix kernel/configs'
+        rm kernel/configs/s/android-5.10/android-base.config
+	curl https://raw.githubusercontent.com/Halhadus/android_kernel_configs/patch-marble/s/android-5.10/android-base.config -o kernel/configs/s/android-5.10/android-base.config
+        rm kernel/configs/s/android-5.10/android-recommended.config
+	curl https://raw.githubusercontent.com/Halhadus/android_kernel_configs/patch-marble/s/android-5.10/android-base.config -o kernel/configs/s/android-5.10/android-recommended.config
